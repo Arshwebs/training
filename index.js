9eb9fb108eb3e9817512e332badf04832e59f29b
Arsh700 <arshathm4@gmail.com>
2023-06-19 12:16:04 +0530

    Project completed

---
+++ index.js
@@ -0,0 +1,46 @@
+// npm install googleapis
+// npm i express
+
+const {google} = require("googleapis");
+const key = require("./diesel-zenith-376015-524413cbc9e6.json");
+
+const client = new google.auth.JWT(key.client_email, null, key.private_key, ["https://www.googleapis.com/auth/spreadsheets"]);
+
+client.authorize(function (err, tokens) {
+	if (err) {
+		console.error("Authentication error:", err);
+		return;
+	}
+	// The client is authorized and ready to write to the spreadsheet.
+	writeDataToSheet(client);
+});
+
+async function writeDataToSheet(auth) {
+	const sheets = google.sheets({version: "v4", auth});
+
+	// Specify the spreadsheet ID and range.
+	const spreadsheetId = "1f597ZHCsCWQMpD8WjojGTc4v9iM8QXC-0Ha_x8zDWk0";
+	const range = "Sheet1!A1:B2";
+
+	// Specify the values to write.
+	const values = [
+		["Name", "Age"],
+		["John Doe", 30],
+	];
+
+	try {
+		// Write the data to the spreadsheet.
+		const response = await sheets.spreadsheets.values.update({
+			spreadsheetId,
+			range,
+			valueInputOption: "RAW",
+			resource: {
+				values,
+			},
+		});
+
+		console.log("Data written successfully.");
+	} catch (err) {
+		console.error("Error writing data:", err);
+	}
+}
